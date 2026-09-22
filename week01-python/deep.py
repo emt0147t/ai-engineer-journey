a9aa93fe0d20a177ddb5de201a32bd180ea66bf1
Nguyễn Xuân Thành <thanhbeo14455@gmail.com>
2026-09-22 20:54:39 +0700

    day2ex1

---
+++ week01-python/deep.py
@@ -0,0 +1,8 @@
+def main():
+    ans=input("What is the Answer to the Great Question of Life, the Universe and Everything? ").strip().lower()
+    if ans=="42" or ans=="forty-two" or ans=="forty two":
+        print("Yes")
+    else: print("No")
+
+if __name__=="__main__":
+    main()
